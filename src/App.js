03e47ed0b51805ac910caf85c27481d02b18abfc
Rizki Putra Wicaksono <rizkiputra1100@gmail.com>
2022-06-18 03:36:52 +0700

    rev: meringkas css

--- src/App.js
+++ src/App.js
@@ -4,17 +4,10 @@ import Contact from "./components/Contact";
 import contacts from "./data/contacts.json";
 
 const App = () => {
-  // console.log(contacts);
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          marginBottom: "5px",
-        }}
-      >
+      <div className="header-container">
         <Header />
         <span
           style={{
@@ -25,19 +18,10 @@ const App = () => {
           ********************************************************************
         </span>
       </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Contact data={contacts[0]} />
-        <Contact data={contacts[1]} />
-        <Contact data={contacts[2]} />
-        <Contact data={contacts[3]} />
-        <Contact data={contacts[4]} />
-        <Contact data={contacts[5]} />
+      <div className="contact-container">
+        {contacts.map((contactss, index) => (
+          <Contact data={contactss} key={index} />
+        ))}
       </div>
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from "react";
-import "./css/contact.css";
+import "./Contact.css";
 
 const Contact = ({ data }) => {
   return (
@@ -10,9 +10,21 @@ const Contact = ({ data }) => {
         <img className="photo" src={data.photo} alt="avatar" />
       </div>
       <div className="details">
-        <h2 style={{ margin: 0 }}>{data.name}</h2>
-        <p style={{ margin: 0 }}>{data.phone}</p>
-        <p style={{ margin: 0 }}>{data.email}</p>
+        <h2>{data.name}</h2>
+        <p>{data.phone}</p>
+        <p>{data.email}</p>
+      </div>
+      <div className="mail">
+        <a href={"mailto:" + data.email} target="_blank" rel="noreferrer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M.026 24l11.974-11.607 11.974 11.607h-23.948zm11.964-23.961l-11.99 8.725v12.476l7.352-7.127-5.653-4.113 10.291-7.488 10.309 7.488-5.655 4.108 7.356 7.132v-12.476l-12.01-8.725z" />
+          </svg>
+        </a>
       </div>
     </div>
   );
